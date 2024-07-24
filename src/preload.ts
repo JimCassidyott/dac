892655681ef95e1c294b958f5aaeb33b66c059ea
Jim Cassidy <jimcassidyott@hotmail.com>
2024-07-24 11:29:54 -0400

    Added some comments to the preload file which is going to be important to us.

--- src/preload.ts
+++ src/preload.ts
@@ -12,3 +12,10 @@ window.addEventListener("DOMContentLoaded", () => {
     replaceText(`${type}-version`, process.versions[type as keyof NodeJS.ProcessVersions]);
   }
 });
+
+//Here you may have to add components and Apis that talk to the file system.
+//For example, you can use the fs module to read the file system and display the contents in the renderer process.
+//You can also use the ipcRenderer module to communicate between the main and renderer processes.
+//You can also use the remote module to access the main process from the renderer process.
+//You can also use the dialog module to display native system dialogs.
+//You can also use the shell module to open external URLs.
