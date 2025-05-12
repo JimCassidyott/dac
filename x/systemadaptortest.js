4c3a14f5bad0c71a472ca0ab541a3f304ed08288
Tharindu H <tharindudeshan500@gmail.com>
2025-05-12 16:08:22 -0400

    Clean up: delete unused code files and folders

--- x/systemadaptortest.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var accessibilityChecker_1 = require("../components/accessibilityChecker");
-console.log((0, accessibilityChecker_1.isAccessible)("a.docx"));
